bb50014f4c71ff4052c1901b71c33ba3c14b96cc
sylvsin <lubwimi@gmail.com>
2020-07-19 19:19:42 +0200

    Updated imagesCollection

--- src/components/Home/imagesCollection.ts
+++ src/components/Home/imagesCollection.ts
@@ -1,8 +1,8 @@
 const imgcollection = [
-    {id: 1, website: 'Articles.nu', imgPath: '/image/aircraft2.jpg', url:"http://articles.nu/"},
-    {id: 2, website: 'Patmos.se', imgPath: '/image/ballons2.jpg', url:"http://www.patmos.se/"},
-    {id: 3, website: 'Lubwimi.com', imgPath: '/image/beach2.jpg', url:"http://lubwimi.com/"},
-    {id: 4, website: 'CV', imgPath: '/image/road2.jpg', url:"http://articles.nu/cv/"},
+    {id: 1, website: 'CV', imgPath: '/image/aircraft2.jpg', url:"http://articles.nu/cv/"},
+    {id: 2, website: 'patmos.se', imgPath: '/image/ballons2.jpg', url:"http://www.patmos.se/"},
+    {id: 3, website: 'articles.nu', imgPath: '/image/beach2.jpg', url:"http://articles.nu/"},
+    {id: 4, website: 'lubwimi.com', imgPath: '/image/road2.jpg', url:"http://lubwimi.com/"},
 ]
 
 export default imgcollection
