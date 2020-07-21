febd2e1189fbacd08c64e623ffebc590c05d3b70
sylvsin <lubwimi@gmail.com>
2020-07-21 15:24:08 +0200

    Changed CV to Todo app link

--- src/components/Home/imagesCollection.ts
+++ src/components/Home/imagesCollection.ts
@@ -1,5 +1,5 @@
 const imgcollection = [
-    {id: 1, website: 'CV', imgPath: '/image/aircraft2.jpg', url:"http://articles.nu/cv/"},
+    {id: 1, website: 'To do app', imgPath: '/image/aircraft2.jpg', url:"https://serene-curran-919f36.netlify.app/"},
     {id: 2, website: 'patmos.se', imgPath: '/image/ballons2.jpg', url:"http://www.patmos.se/"},
     {id: 3, website: 'articles.nu', imgPath: '/image/beach2.jpg', url:"http://articles.nu/"},
     {id: 4, website: 'lubwimi.com', imgPath: '/image/road2.jpg', url:"http://lubwimi.com/"},
