77792b0b6164e98c2a05432a84fa80134b72f67f
sylvsin <lubwimi@gmail.com>
2020-07-24 09:25:50 +0200

    Updated imagesCollection.ts

--- src/components/Home/imagesCollection.ts
+++ src/components/Home/imagesCollection.ts
@@ -1,7 +1,7 @@
 const imgcollection = [
     {id: 1, website: 'To do app', imgPath: '/image/aircraft2.jpg', url:"https://dreamy-chandrasekhar-ee710d.netlify.app/"},
-    {id: 2, website: 'patmos.se', imgPath: '/image/ballons2.jpg', url:"http://www.patmos.se/"},
-    {id: 3, website: 'articles.nu', imgPath: '/image/beach2.jpg', url:"http://articles.nu/"},
+    {id: 2, website: 'Webshop', imgPath: '/image/ballons2.jpg', url:"https://tender-bhabha-50f652.netlify.app/"},
+    {id: 3, website: 'patmos.se', imgPath: '/image/beach2.jpg', url:"http://www.patmos.se/"},
     {id: 4, website: 'lubwimi.com', imgPath: '/image/road2.jpg', url:"http://lubwimi.com/"},
 ]
 
